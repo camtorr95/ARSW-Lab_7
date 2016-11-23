1dfb25609dbc551d210ffa79ca3c4f31fbf1a417
Hector <hector.cadavid@gmail.com>
2016-11-23 11:55:13 -0500

    documentacion agregada

--- src/main/java/edu/eci/arsw/msgbroker/services/GameStatePersistence.java
+++ src/main/java/edu/eci/arsw/msgbroker/services/GameStatePersistence.java
@@ -23,13 +23,44 @@ import edu.eci.arsw.msgbroker.model.HangmanGame;
  * @author hcadavid
  */
 public interface GameStatePersistence {
-
+    
+    /**
+     * Crea y hace persitente el estado de un nuevo juego, con la palabra
+     * indicada
+     * @param id identificador del juego
+     * @param word palabra del juego
+     * @throws GameCreationException si se está usando un identificador
+     * que no esta disponible
+     */
     public void createGame(int id,String word) throws GameCreationException;
     
+    /**
+     * Consultar un juego, dado su identificador
+     * @param gameid el identificador del juego
+     * @return una representacion en objetos del juego del identificador dado
+     * @throws GameNotFoundException si no hay un juego con el identificador dado
+     */
     public HangmanGame getGame(int gameid) throws GameNotFoundException;
     
+    /**
+     * Agrega una letra al juego actual.
+     * @param gameid el identificador del juego
+     * @param c la letra a ser agregada
+     * @throws GameNotFoundException si no hay un juego con el identificador dado
+     */
     public void addLetter(int gameid, char c) throws GameNotFoundException;
     
+    /**
+     * Realiza un intento de adivinar la palabra del juego indicado
+     * @param gameid identificador del juego
+     * @param player nombre del jugador que realiza el intento
+     * @param word la palabra adivinada
+     * @return true si la palabra es la correcta
+     * @pos si se adivino correctamente, se debe cambiar el estado del juego
+     *      a finalizado, y se debe asignar el jugador indicado como 
+     *      ganador
+     * @throws GameNotFoundException si no hay un juego con el identificador dado
+     */
     public boolean checkWord(int gameid, String player,String word) throws GameNotFoundException ;
     
 }
